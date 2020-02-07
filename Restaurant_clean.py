c640f344997407ea4dc78d7e8ace46972e0869b4
Denisiuskley <denispstu@mail.ru>
2020-02-07 15:15:26 +0500

    3 commit

--- Restaurant_clean.py
+++ Restaurant_clean.py
@@ -15,11 +15,6 @@ from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'qt')
 
 df = pd.read_csv('main_task.xls')
-Capitals = pd.read_csv('Capitals.csv', sep = ';')
-df = pd.merge(df, Capitals, on='City', how='left')
-df = df.drop(['Country'], axis = 1)
-df['Capital'] = df['Capital'].fillna(0)
-
 
 '''Замена символов на коды в ценовом диапазоне'''
 df.loc[df['Price Range'] == '$', 'Price ID'] = 1
@@ -36,6 +31,39 @@ df['Ranking norm'] = 6 - df['Ranking norm']
 pattern = re.compile('\'\w+')
 df['Num Cuisine'] = df['Cuisine Style'].apply(lambda x: 1 if pd.isna(x) 
         else len(pattern.findall(x)))
+n = df['Cuisine Style'].tolist()
+id_ta = df['ID_TA'].tolist()
+city = df['City'].tolist()
+m = []
+for i in range(len(n)):
+    try:
+        l = n[i][1:-1].split(', ')
+        for j in l:
+            m.append([id_ta[i],city[i],j[1:-1]])
+    except: pass
+        
+Cuisine = pd.DataFrame(m, columns = ['ID_TA', 'City', 'Cuisine'])
+pivot = Cuisine.pivot_table(values=['ID_TA'],
+                       index=['City'],
+                       columns=['Cuisine'],
+                       aggfunc='count',
+                       fill_value=0)
+col = pivot.index
+pivot = Cuisine.pivot_table(values=['ID_TA'],
+                       index=['Cuisine'],
+                       columns=['City'],
+                       aggfunc='count',
+                       fill_value=0)
+pivot.columns = col
+
+for i in range(len(m)):
+    try:
+        m[i].append(pivot.loc[m[i][2] , m[i][1]])
+    except: pass
+
+Cuisine = pd.DataFrame(m, columns = ['ID_TA', 'City', 'Cuisine', 'Num cuisines in City'])
+Cuisine_gr = Cuisine.groupby(['ID_TA'])['Num cuisines in City'].sum().sort_values(ascending=False)
+df = pd.merge(df, Cuisine_gr, on='ID_TA', how='left')
 
 '''Обработка отзывов. Выберем все слова в один массив, а даты в другой'''
 pattern = re.compile('\w+')
@@ -60,7 +88,7 @@ for index, row in df.iterrows():
 '''Выбираем наиболее популярные слова и создаем метрики положительного и отрицательного окраса.'''
 Words = pd.DataFrame(k, columns = ['ID_TA', 'Word','Ranking norm'])
 Words_value = Words['Word'].value_counts().reset_index()
-Words_rating = Words.groupby(['Word'])['Ranking norm'].mean().reset_index()
+Words_rating = Words.groupby(['Word'])['Ranking norm'].sum().reset_index()
 Words_value.columns = ['Word','Num']
 Words_value = pd.merge(Words_value, Words_rating, on='Word', how='left')
 
@@ -125,28 +153,37 @@ df['Period'] = df['Period'].apply(lambda x: x.days)
 '''Вычисление параметра разницы между ID ресторана и местом в городе'''
 df['dif Rank rID'] = df['Restaurant_id'].apply(lambda x: int(x[3:])) - df['Ranking']
 
-'''Исключение пропусков'''
-df['ID Word'] = df.groupby('City')['ID Word'].transform(lambda x: x.fillna(x.mean()))
-df['Period'] = df.groupby('City')['Period'].transform(lambda x: x.fillna(x.mean()))
-df['Sec to Now'] = df.groupby('City')['Sec to Now'].transform(lambda x: x.fillna(x.median()))
 
+'''Исключение пропусков'''
+df = df.sort_values('Ranking norm')
+df['Num cuisines in City_isNAN'] = pd.isna(df['Num cuisines in City']).astype('uint8')
+df['Num cuisines in City'] = df.groupby('City')['Num cuisines in City'].transform(lambda x: x.fillna(x.median()))
+df['ID Word_isNAN'] = pd.isna(df['ID Word']).astype('uint8')
+df['ID Word'] = df.groupby('City')['ID Word'].transform(lambda x: x.fillna(x.mean()))
+df['Period_isNAN'] = pd.isna(df['Period']).astype('uint8')
+df['Period'] = df['Period'].fillna(method = 'backfill')
+df['Sec to Now_isNAN'] = pd.isna(df['Sec to Now']).astype('uint8')
+df['Sec to Now'] = df['Sec to Now'].fillna(method = 'backfill')
+df['Number of Reviews_isNAN'] = pd.isna(df['Number of Reviews']).astype('uint8')
 df.loc[(pd.isna(df['Number of Reviews'])) & (df['Period'] == 0), 'Number of Reviews'] = 1
 df.loc[pd.isna(df['Number of Reviews']), 'Number of Reviews'] = 0
-
-df['Number of Reviews otn'] = df.groupby('City')['Number of Reviews'].transform(
-        lambda x: x / x.max())
-df['Number of Reviews otn'] = df['Number of Reviews otn'].fillna(0)
+df['Price ID_isNAN'] = pd.isna(df['Price ID']).astype('uint8')
 df['Price ID'] = df['Price ID'].fillna(2)
-df['Date first review'] = df.groupby('City')['Date first review'].transform(
-        lambda x: x.fillna(x.mean()))
-df['Date second review'] = df.groupby('City')['Date second review'].transform(
-        lambda x: x.fillna(x.mean()))
+
+'''Дополнительные метрики, хорошо коррелирующие со средними по городам рейтингами'''
+df['ID Word mean City'] = df.groupby('City')['ID Word'].transform(lambda x: x.mean())
+df['Price ID mean City'] = df.groupby('City')['Price ID'].transform(lambda x: x.mean())
+df['Word/Price'] = df['ID Word'] / df['Price ID']
+df['Word/Price mean City'] = df['ID Word mean City'] / df['Price ID mean City']
+
+df['ID Word mean City rank'] = df['ID Word mean City'] * df['Ranking norm']
+df['Price ID mean City rank'] = df['Price ID mean City'] * df['Ranking norm']
+df['Word/Price mean City rank'] = df['Word/Price mean City'] * df['Ranking norm']
 
 '''Создание признаков dummy variables'''
 '''Метрики городов (немного уменьшает ошибку)'''
 df = pd.concat([df, pd.get_dummies(df['City'])], axis=1, sort=False)
 
-   
 iskl = ['Restaurant_id', 'Rating','City','Cuisine Style','Price Range',
          'Reviews','URL_TA','ID_TA','Date first review','Date second review','Ranking']
 
@@ -154,12 +191,10 @@ X = df.drop(iskl, axis = 1)
 y = df['Rating']
 
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-
-from sklearn.ensemble import RandomForestRegressor # инструмент для создания и обучения модели
-from sklearn import metrics # инструменты для оценки точности модели
-
 regr = RandomForestRegressor(n_estimators=100, n_jobs=-1)
 regr.fit(X_train, y_train)
 y_pred = regr.predict(X_test)
